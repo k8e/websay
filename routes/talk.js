31758eecda515d01a41a75fada5c0a1ea6204337
k8e <k8eSchaffer@gmail.com>
2017-01-17 01:02:10 -0600

    Log change

--- routes/talk.js
+++ routes/talk.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  console.log(req.ip + " : \t\"" + req.query.t + "\"");
+  console.log("SHARE (" + req.ip + ") : \t\"" + req.query.t + "\"");
   res.render('talk', { title: 'Talk page', input: req.query.t, rate: req.query.r, pitch: req.query.p, voice: req.query.v, });
 });
 
